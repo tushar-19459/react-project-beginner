903502b935ed70cb91c06e03b9231b093725988d
tushar-19459 <19459tusharraj@gmail.com>
2025-06-10 13:21:48 +0530

    setup food

---
+++ food-recipy-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Food from "./components/Food"
+function App() {
+
+  return (
+    <>
+      <Food></Food>
+    </>
+  )
+}
+
+export default App
---
+++ food-recipy-app/src/components/Food.jsx
@@ -0,0 +1,5 @@
+export default function Food(){
+ return(
+    <div>this is food</div>
+ )   
+}
